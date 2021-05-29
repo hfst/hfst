92f723936ef33d41abe6c43c46f2c600a93a465d
Victor Mireyev <victor@opennodecloud.com>
2021-05-29 18:14:17 +0300

    Drop extra brackets.

--- back-ends/foma/constructions.c
+++ back-ends/foma/constructions.c
@@ -914,8 +914,8 @@ void fsm_merge_sigma(struct fsm *net1, struct fsm *net2) {
 
   sigmasizes = sigma_max(sigma_1) + sigma_max(sigma_2) + 3;
 
-  mapping_1 = xxmalloc(sizeof(int) * (sigmasizes));
-  mapping_2 = xxmalloc(sizeof(int) * (sigmasizes));
+  mapping_1 = xxmalloc(sizeof(int) * sigmasizes);
+  mapping_2 = xxmalloc(sizeof(int) * sigmasizes);
 
   /* Fill mergesigma */
 
