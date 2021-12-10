de513241067a9be85c5f7657b3e2db12ff600bc3
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2021-12-10 15:37:03 +0100

    minimaler test

--- test/libhfst/test_pmatch.cc
+++ test/libhfst/test_pmatch.cc
@@ -1,13 +1,12 @@
 #include "implementations/optimized-lookup/pmatch.h"
 #include "implementations/optimized-lookup/pmatch_tokenize.h"
-#include "auxiliary_functions.cc"
-
-using namespace hfst_ol;
 
 int main(int argc, char **argv)
 {
-  verbose_print("PmatchContainer load");
-  std::ifstream instream("./tokeniser.pmhfst");
-  auto * t = new hfst_ol::PmatchContainer(instream);
+	std::cerr << "loading " << std::endl;
+	std::ifstream s = std::ifstream("tokeniser.pmhfst");
+	std::unique_ptr<hfst_ol::PmatchContainer> tok(new hfst_ol::PmatchContainer(s));
+	std::cerr << "loaded " << std::endl;
+        return EXIT_SUCCESS;
 }
 
